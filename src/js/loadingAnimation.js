4b8fb632e5f9556937dc7670715b17a24f102f70
Solpapaya <dsolorioparedes@gmail.com>
2021-03-31 05:09:58 -0600

    Loading Page Animation Already Works for Firefox
    I have added the snap.svg library for animating the SVG. This makes the animation possible in Firefox because before using the library I was using the 'd' attribute in CSS and that only works for Chrome. So, now the snap.svg library takes care of the animation. And also, I've tried to use the gsap library for animating the Loading Square but it was more complicated than using CSS keyframes, so I left the code I tried to use with gsap in the 'loadingAnimation.js' file (it is commented)

--- src/js/loadingAnimation.js
+++ src/js/loadingAnimation.js
@@ -1,20 +1,42 @@
-function createLoadingAnimation() {
-    const loadingContainer = document.querySelector('.loading-container');
-    loadingContainer.classList.add('show');
+async function createLoadingAnimation() {
+    const path = Snap(document.querySelector('.load'));
+    const finalPath = "M 0,0 c 0,0 63.5,-16.5 80,0 16.5,16.5 0,60 0,60 L 0,60 Z";
+    await showLoadingContainer();
+    path.animate( { 'path' : finalPath }, 1000, mina.linear);
 
-    // const loadingSquare = document.querySelector('.loading-animation');
+    const squareLoadingAnimation = document.querySelector('.loading-animation');
+    squareLoadingAnimation.addEventListener('mouseover', function() {
+        this.classList.add('show-error-message');
+    })
 
-    // loadingSquare.addEventListener('mouseover', function(e) {
-    //     this.classList.add('show-error-message');
-    //     this.firstElementChild.classList.add('show-text-message');
-    //     // this.classList.add('show-success-message');
-    //     // setTimeout(() => {
-    //     //     this.textContent = "Hello";
-    //     // }, 1000)
+
+    // const masterTimeline = gsap.timeline({defaults: {ease: "power1.out"}});
+    // const infiniteRotation = gsap.timeline({repeat: -1, defaults: {ease: "power1.out"}});
+    
+    // masterTimeline.to('.loading-animation', {opacity: 1, transform: "rotateX(180deg)", duration: 0.5, delay: 0.7})
+    // masterTimeline.add(infiniteRotation);
+
+    // infiniteRotation.fromTo('.loading-animation', {transform: "rotateY(0deg)"}, {transform: "rotateY(180deg)", transformOrigin: "center", duration: 0.5})
+    // infiniteRotation.fromTo('.loading-animation', {transform: "rotateX(0deg)"}, {transform: "rotateX(180deg)", duration: 0.5})
+
+    // const squareLoadingAnimation = document.querySelector('.loading-animation');
+    // squareLoadingAnimation.addEventListener('mouseover', () => {
+    //     // infiniteRotation.set({clearProps: "transform"}) 
+    //     infiniteRotation.progress(0).clear()
+    //     masterTimeline.to('.loading-animation', {width: "20rem", duration: 3});
     // })
 }
 
+function showLoadingContainer() {
+    const loadingContainer = document.querySelector('.loading-container');
+    loadingContainer.classList.add('show');    
+}
+
 function deleteLoadingAnimation() {
+    const path = Snap(document.querySelector('.load'));
+    const startingPath = "M 0,0 c 0,0 -16.5,43.5 0,60 16.5,16.5 80,0 80,0 L 0,60 Z";
+    path.animate( { 'path' : startingPath }, 1000, mina.linear);
+
     const loadingContainer = document.querySelector('.loading-container');
     loadingContainer.classList.add('hide');
     setTimeout(() => {
